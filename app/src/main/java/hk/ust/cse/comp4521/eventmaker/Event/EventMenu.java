1e6fdb292446a266dff542d4af14d432e9169fc7
Li Lok Him <lilokhim@hotmail.com>
2015-05-14 19:18:00 +0800

    Him

--- app/src/main/java/hk/ust/cse/comp4521/eventmaker/Event/EventMenu.java
+++ app/src/main/java/hk/ust/cse/comp4521/eventmaker/Event/EventMenu.java
@@ -7,9 +7,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import hk.ust.cse.comp4521.eventmaker.Constants;
 import hk.ust.cse.comp4521.eventmaker.R;
+import hk.ust.cse.comp4521.eventmaker.User.UserInfo;
+import hk.ust.cse.comp4521.eventmaker.User.UserServer;
 
 /**
  * Created by LiLokHim on 14/5/15.
@@ -51,13 +55,17 @@ public class EventMenu extends Activity {
         user2_button.setVisibility(View.INVISIBLE);
         user3_button.setVisibility(View.INVISIBLE);
         user4_button.setVisibility(View.INVISIBLE);
+        user1_button.setOnClickListener(new pressButton());
+        user2_button.setOnClickListener(new pressButton());
+        user3_button.setOnClickListener(new pressButton());
+        user4_button.setOnClickListener(new pressButton());
 
         /************************UI init*******************/
 
 
 
         Intent intent = getIntent();
-//        event_id = intent.getExtras().getString();
+        event_id = intent.getExtras().getString(Constants.eventId);
         Thread get_event_thread=new get_my_event();
         get_event_thread.start();
 
@@ -71,17 +79,67 @@ public class EventMenu extends Activity {
             event_name.setText(event.interest);
             time_text.setText(event.starting+"-"+event.ending);
             location_text.setText(event.locationName);
+            List<UserInfo> event_users=get_users_in_event();
 
+            for(int i=0;i<event_users.size();i++)
+            {
+                user1_button.setVisibility(View.INVISIBLE);
+                user2_button.setVisibility(View.INVISIBLE);
+                user3_button.setVisibility(View.INVISIBLE);
+                user4_button.setVisibility(View.INVISIBLE);
+
+
+                if(i==0)
+                {
+                    user1_button.setVisibility(View.VISIBLE);
+                    user1_button.setText(event_users.get(i).Name);
+                }
+                else if(i==1)
+                {
+                    user2_button.setVisibility(View.VISIBLE);
+
+                    user2_button.setText(event_users.get(i).Name);
+
+                }
+                else if(i==2)
+                {
+                    user3_button.setVisibility(View.VISIBLE);
+
+                    user3_button.setText(event_users.get(i).Name);
+
+                }
+                else if(i==3)
+                {
+                    user4_button.setVisibility(View.VISIBLE);
+
+                    user4_button.setText(event_users.get(i).Name);
+
+                }
+            }
         }
     }
 
-//    public List<UserInfo> get_users_in_event()
-//    {
-//        List<UserInfo> allUsers= UserServer.UserInfoArrayList;
-//
-//
-//
-//    }
+
+    //Get all the users from the event
+    public List<UserInfo> get_users_in_event()
+    {
+        List<UserInfo> allUsers= UserServer.UserInfoArrayList;
+        List<UserInfo> ret_list=new ArrayList<UserInfo>();
+
+        //find the owner info
+        for(UserInfo user:allUsers) {
+            if (event._ownerid.equals(user._id))
+            {
+                ret_list.add(user);
+                break;
+            }
+        }
+        //find the others
+
+
+        return ret_list;
+
+    }
 
     //check the event from test and update the event periodically, also call update_info every time
     public class get_my_event extends Thread {
@@ -90,7 +148,7 @@ public class EventMenu extends Activity {
 
             for(Event events:allEvents)
             {
-                if(events._id==event_id){
+                if(events._id.equals(event_id)){
                 event=events;
                     update_info();
                     break;
@@ -107,6 +165,22 @@ public class EventMenu extends Activity {
 
 
     }
+    public class pressButton implements View.OnClickListener {
 
+        @Override
+        public void onClick(View view) {
+            if (view.getId() == R.id.user1_button) {
+            }
+            else if(view.getId()==R.id.user2_button){
+
+            }
+            else if(view.getId()==R.id.user3_button){
+
+            }
+            else if(view.getId()==R.id.user4_button){
+
+            }
+        }
+    }
 
 }
